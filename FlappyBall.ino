8a6cb0619e394fc6e31755a106315f56b5d414f8
jacksonjude <jacksonjude@mac.com>
2020-09-12 23:23:04 -0700

    Added EEPROM save files for high scores; System and sketch audio distinction

--- FlappyBall.ino
+++ FlappyBall.ino
@@ -35,9 +35,17 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ------------------------------------------------------------------------------
 */
 
+/*
+Save File fork by jacksonjude
+copyright (c) 2020
+*/
+
 #include <Arduboy2.h>
 #include <ArduboyTones.h>
 #include "bitmaps.h"
+
+#include <SPI.h>
+#include <EEPROM.h>
 
 Arduboy2 arduboy;
 ArduboyTones sound(arduboy.audio.enabled);
@@ -93,16 +101,11 @@ char ballFlapper = BALL_RADIUS; // Floaty's wing length
 char gameScoreX = 0;
 char gameScoreY = 0;
 byte gameScoreRiser = 0;
+int rightButtonLoopCount = 0;
+char unitName[ARDUBOY_UNIT_NAME_LEN + 1];
+boolean inLoadScreen = false;
 
 // Sounds
-const uint16_t intro[] PROGMEM = {
-  NOTE_C5,500, NOTE_C4,500, NOTE_E4,500, NOTE_A4,500, NOTE_G4,500, NOTE_C4,500,
-  NOTE_C5,500, NOTE_C4,500, NOTE_E4,500, NOTE_A4,500, NOTE_G4,500, NOTE_C4,500,
-  NOTE_A4,500, NOTE_A3,500, NOTE_C4,500, NOTE_F4,500, NOTE_D4,500, NOTE_A3,500,
-  NOTE_A4,500, NOTE_A3,500, NOTE_C4,500, NOTE_G4,500, NOTE_D4,500, NOTE_F4,500,
-  NOTE_C5, 2000, TONES_END
-};
-
 const uint16_t bing[] PROGMEM = {
   NOTE_FS1,107, NOTE_C4,500, TONES_END
 };
@@ -121,13 +124,53 @@ const uint16_t hit[] PROGMEM = {
   NOTE_C4,31, NOTE_CS4,31, NOTE_D4,31, TONES_END
 };
 
+// EEPROM Locations
+#define EEPROM_DATA_SETUP_ADDRESS 499
+#define CURRENT_HIGH_SCORE_ADDRESS 500
+#define SKETCH_AUDIO_ENABLED_ADDRESS 501
+#define LOADED_FILE_NUMBER_ADDRESS 502
+#define FILE_1_SCORE_ADDRESS 503
+#define FILE_2_SCORE_ADDRESS 504
+#define FILE_3_SCORE_ADDRESS 505
+
 void setup() {
+  Serial.begin(9600);
   arduboy.begin();
   arduboy.setFrameRate(FRAMES_PER_SECOND);
+
+  loadFileHighScore();
+
+  if (EEPROM.read(EEPROM_DATA_SETUP_ADDRESS) == 1) {          //If the data setup is done already
+    gameHighScore = EEPROM.read(CURRENT_HIGH_SCORE_ADDRESS);   //Read the game high score from data
+
+    Serial.println("High Score Loaded: " + String(EEPROM.read(CURRENT_HIGH_SCORE_ADDRESS)));
+    Serial.println("Audio Loaded: " + String(getSketchAudioEnabled()));
+
+    if (arduboy.audio.enabled())
+    {
+      arduboy.audio.on();
+    }
+
+    if (getSketchAudioEnabled()) {      // If sound is enabled in the data
+      Serial.println("Audio ON");
+    } else {
+      sound.noTone();                 // Stop anything that's playing
+    	Serial.println("Audio OFF");
+    }
+  } else {
+    EEPROM.write(EEPROM_DATA_SETUP_ADDRESS, 1);				  //Write default settings
+    EEPROM.write(CURRENT_HIGH_SCORE_ADDRESS, 0);
+    EEPROM.write(SKETCH_AUDIO_ENABLED_ADDRESS, 1);
+    EEPROM.write(LOADED_FILE_NUMBER_ADDRESS, 1);
+    EEPROM.write(FILE_1_SCORE_ADDRESS, 0);
+    EEPROM.write(FILE_2_SCORE_ADDRESS, 0);
+    EEPROM.write(FILE_3_SCORE_ADDRESS, 0);
+  }
+
+  delay(1000);
   arduboy.clear();
   arduboy.drawSlowXYBitmap(0,0,floatyball,128,64,1);
   arduboy.display();
-  sound.tones(intro);
   delay(500);
   arduboy.setCursor(18,55);
   arduboy.print("Press Any Button");
@@ -145,13 +188,17 @@ void loop() {
     return;
 
   if (arduboy.pressed(LEFT_BUTTON)) { // If the button for sound toggle is pressed
-    if (arduboy.audio.enabled()) {    // If sound is enabled
+    boolean audioEnabled = !(getSketchAudioEnabled());
+    setSketchAudioEnabled(audioEnabled);
+
+    if (!audioEnabled) {              // If sound is enabled
       sound.noTone();                 // Stop anything that's playing
-      arduboy.audio.off();            // Mute sounds
     } else {
-      arduboy.audio.on();             // Enable sound
-      sound.tones(bing);              // Play a sound to indicate sound has been turned on
+      playTones(bing);                // Play a sound to indicate sound has been turned on
     }
+
+    Serial.println("Audio Enabled: " + String(audioEnabled));
+
     debounceButtons();                // Wait for button release
   }
 
@@ -167,6 +214,61 @@ void loop() {
       gameState = 1;        // Then start the game
       sound.noTone();       // Stop any playing sound
       beginJump();          // And make Floaty jump
+    }
+
+    if (arduboy.pressed(B_BUTTON))
+    {
+    	drawLoadScreen();
+    }
+
+    if (arduboy.pressed(RIGHT_BUTTON))
+    {
+    	rightButtonLoopCount++;
+    	Serial.println(rightButtonLoopCount);
+    	if (rightButtonLoopCount > 150)
+    	{
+    		debounceButtons();
+    		refreshEraseScreen();
+			  int choiceSelected = 2;
+			  arduboy.drawSlowXYBitmap(6,(choiceSelected*10) + 7,arrow,8,6,1);
+
+			  arduboy.display();
+
+  			while (!arduboy.pressed(RIGHT_BUTTON))
+  			{
+  				if (arduboy.pressed(DOWN_BUTTON) || arduboy.pressed(UP_BUTTON))
+  				{
+  					refreshEraseScreen();
+
+  					choiceSelected++;
+  					if (choiceSelected > 2)
+  					{
+  						choiceSelected = 1;
+  					}
+
+  					arduboy.drawSlowXYBitmap(6,(choiceSelected*10) + 7,arrow,8,6,1);
+
+  					arduboy.display();
+  					debounceButtons();
+  				}
+  			}
+
+  			if (choiceSelected == 1)
+  			{
+  				resetCurrentFile();
+  			}
+
+  			rightButtonLoopCount = 0;
+  			arduboy.clear();
+
+  			drawFloor();
+    		drawFloaty();
+    		drawInfo();
+    	}
+    }
+    else
+    {
+    	rightButtonLoopCount = 0;
     }
   }
 
@@ -200,7 +302,7 @@ void loop() {
           gameScoreX = BALL_X;                  // Load up the floating text with
           gameScoreY = ballY - BALL_RADIUS - 8; //  current ball x/y values
           gameScoreRiser = 15;          // And set it for 15 frames
-          sound.tones(point);
+          playTones(point);
         }
       }
     }
@@ -237,13 +339,31 @@ void loop() {
       pipeGap--;
       pipeReduceCount = PIPE_GAP_REDUCE;  // Restart the countdown
     }
+
+    if (arduboy.pressed(RIGHT_BUTTON))
+    {
+    	arduboy.drawRect(56,28,16,16, WHITE);
+    	arduboy.fillRect(57,29,14,14, BLACK);
+
+    	arduboy.fillRect(61,32,2,8, WHITE);
+    	arduboy.fillRect(65,32,2,8, WHITE);
+
+    	arduboy.display();
+
+  		debounceButtons();
+  		while (!arduboy.pressed(RIGHT_BUTTON)) { }
+  		debounceButtons();
+  	}
+
+  	arduboy.setCursor(0, 0);
+  	arduboy.print(gameScore);
   }
 
   // ===== State: Game Over =====
   if (gameState == 2) {  // If the gameState is 2 then we draw a Game Over screen w/ score
     if (gameScore > gameHighScore) { gameHighScore = gameScore; }
     arduboy.display();              // Make sure final frame is drawn
-    sound.tones(hit);               // Hit sound
+    playTones(hit);                 // Hit sound
     delay(100);                     // Pause for the sound
     startFalling();                 // Start falling from current position
     while (ballY + BALL_RADIUS < (HEIGHT-1)) {  // While floaty is still airborne
@@ -255,28 +375,32 @@ void loop() {
       arduboy.display();
       while (!arduboy.nextFrame()) { }  // Wait for next frame
     }
-    sound.tones(horns);                  // Sound the loser's horn
+    playTones(horns);                    // Sound the loser's horn
     arduboy.drawRect(16,8,96,48, WHITE); // Box border
     arduboy.fillRect(17,9,94,46, BLACK); // Black out the inside
     arduboy.drawSlowXYBitmap(30,12,gameover,72,14,1);
-    arduboy.setCursor(50 - getOffset(gameScore),30);
+    arduboy.setCursor(56 - getOffset(gameScore),30);
     arduboy.print(gameScore);
-    arduboy.setCursor(62,30);
+    arduboy.setCursor(69,30);
     arduboy.print("Score");
 
-    arduboy.setCursor(50 - getOffset(gameHighScore),42);
+    arduboy.setCursor(56 - getOffset(gameHighScore),42);
     arduboy.print(gameHighScore);
-    arduboy.setCursor(62,42);
+    arduboy.setCursor(69,42);
     arduboy.print("High");
 
+    EEPROM.write(CURRENT_HIGH_SCORE_ADDRESS, gameHighScore);
+    int fileNumber = EEPROM.read(LOADED_FILE_NUMBER_ADDRESS);
+	  Serial.println(LOADED_FILE_NUMBER_ADDRESS + fileNumber);
+	  EEPROM.write(LOADED_FILE_NUMBER_ADDRESS + fileNumber, gameHighScore);
+
     arduboy.display();
-    delay(1000);         // Give some time to stop pressing buttons
+    delay(500);         // Give some time to stop pressing buttons
 
     while (!jumpPressed()) { } // Wait for a jump button to be pressed
     debounceButtons();
 
     gameState = 0;       // Then start the game paused
-    sound.tones(intro);  // Play the intro
     gameScore = 0;       // Reset score to 0
     gameScoreRiser = 0;  // Clear the floating score
     for (byte x = 0; x < PIPE_ARRAY_SIZE; x++) { pipes[0][x] = 0; }  // set all pipes inactive
@@ -293,11 +417,15 @@ void drawInfo() {
   byte ulLength;    // Length of underline
 
   arduboy.setCursor(6, 3);
-  arduboy.print("A,B,Up,Down: Jump");
+  arduboy.print("A, Up: Jump");
   arduboy.setCursor(6, 51);
   arduboy.print("Left: Sound On/Off");
+  arduboy.setCursor(6, 13);
+  arduboy.print("Right: Pause/Select");
+  arduboy.setCursor(6, 41);
+  arduboy.print("B: Load File");
 
-  if (arduboy.audio.enabled()) {
+  if (getSketchAudioEnabled()) {
     ulStart = 13 * 6;
     ulLength = 2 * 6 - 1;
   } else {
@@ -352,8 +480,8 @@ void generatePipe() {
 boolean checkPipe(byte x) {  // Collision detection, x is pipe to check
   byte AxA = BALL_X - (BALL_RADIUS-1);  // Hit box for floaty is a square
   byte AxB = BALL_X + (BALL_RADIUS-1);  // If the ball radius increases too much, corners
-  byte AyA = ballY - (BALL_RADIUS-1);  // of the hitbox will go outside of floaty's
-  byte AyB = ballY + (BALL_RADIUS-1);  // drawing
+  byte AyA = ballY - (BALL_RADIUS-1);   // of the hitbox will go outside of floaty's
+  byte AyB = ballY + (BALL_RADIUS-1);   // drawing
   byte BxA, BxB, ByA, ByB;
 
   // check top cylinder
@@ -386,12 +514,12 @@ boolean checkPipe(byte x) {  // Collision detection, x is pipe to check
 }
 
 boolean jumpPressed() { // Return "true" if a jump button is pressed
-  return (arduboy.buttonsState() & (UP_BUTTON | DOWN_BUTTON | A_BUTTON | B_BUTTON)) != 0;
+  return (arduboy.buttonsState() & (UP_BUTTON | A_BUTTON)) != 0;
 }
 
 void beginJump() {
   if (!sound.playing()) {   // Play "flap" sound only if nothing is playing
-   sound.tone(NOTE_C1, 125);
+   playTone(NOTE_C1, 125);
   }
   ballV = BALL_JUMP_VELOCITY;
   ballFrame = 0;
@@ -423,11 +551,275 @@ void debounceButtons() { // prevent "noisy" buttons from appearing as multiple p
   delay(100);
 }
 
-byte getOffset(unsigned int s) {
-  if (s > 9999) { return 24; }
-  if (s > 999) { return 18; }
-  if (s > 99) { return 12; }
-  if (s > 9) { return 6; }
+byte getOffset(byte s) {
+  if (s > 9999) { return 20; }
+  if (s > 999) { return 15; }
+  if (s > 99) { return 10; }
+  if (s > 9) { return 5; }
   return 0;
 }
 
+void playTones(uint16_t tones[])
+{
+  if (!arduboy.audio.enabled() || !getSketchAudioEnabled()) { return; }
+
+  sound.tones(tones);
+}
+
+void playTone(uint16_t note, int length)
+{
+  if (!arduboy.audio.enabled() || !getSketchAudioEnabled()) { return; }
+
+  sound.tone(NOTE_C1, length);
+}
+
+boolean getSketchAudioEnabled()
+{
+  return EEPROM.read(SKETCH_AUDIO_ENABLED_ADDRESS) == 1;
+}
+
+void setSketchAudioEnabled(boolean audioEnabled)
+{
+  EEPROM.write(SKETCH_AUDIO_ENABLED_ADDRESS, audioEnabled);
+}
+
+void drawLoadScreen()
+{
+  inLoadScreen = true;
+	refreshLoadScreen();
+	arduboy.drawSlowXYBitmap(6,(EEPROM.read(LOADED_FILE_NUMBER_ADDRESS)*10) + 7,arrow,8,6,1);
+	arduboy.display();
+
+	debounceButtons();
+
+	int fileSelected = EEPROM.read(LOADED_FILE_NUMBER_ADDRESS);
+	while (!arduboy.pressed(RIGHT_BUTTON))
+	{
+		if (arduboy.pressed(DOWN_BUTTON))
+		{
+			refreshLoadScreen();
+			fileSelected++;
+			if (fileSelected > 3)
+			{
+				fileSelected = 1;
+			}
+
+			arduboy.drawSlowXYBitmap(6,(fileSelected*10) + 7,arrow,8,6,1);
+
+			arduboy.display();
+			debounceButtons();
+		}
+
+    if (arduboy.pressed(UP_BUTTON))
+    {
+      refreshLoadScreen();
+      fileSelected--;
+      if (fileSelected < 1)
+      {
+        fileSelected = 3;
+      }
+
+      arduboy.drawSlowXYBitmap(6,(fileSelected*10) + 7,arrow,8,6,1);
+
+      arduboy.display();
+      debounceButtons();
+    }
+
+    if (arduboy.pressed(LEFT_BUTTON))
+    {
+      drawNameSelectionScreen(fileSelected);
+    }
+	}
+
+	EEPROM.write(LOADED_FILE_NUMBER_ADDRESS, fileSelected);
+	loadFileHighScore();
+
+  inLoadScreen = false;
+
+	debounceButtons();
+}
+
+void refreshLoadScreen()
+{
+	arduboy.clear();
+
+	int file1 = EEPROM.read(FILE_1_SCORE_ADDRESS);
+	int file2 = EEPROM.read(FILE_2_SCORE_ADDRESS);
+	int file3 = EEPROM.read(FILE_3_SCORE_ADDRESS);
+
+	arduboy.setCursor(6, 3);
+	arduboy.print("High Scores");
+	arduboy.drawFastHLine(5, 11, 67, WHITE);
+
+  printFile(false, 0, 0, 1);
+  printFile(true, 506, 510, 2);
+  printFile(true, 511, 515, 3);
+}
+
+void printFile(boolean memory, int adressOn, int adressEnd, int file)
+{
+  arduboy.setCursor(16, file*10+6);
+  arduboy.print(String(file) + ":" + String(EEPROM.read(CURRENT_HIGH_SCORE_ADDRESS+file+2)));
+
+  String fileName;
+  if (memory == true)
+  {
+    while (adressOn < adressEnd+1)
+    {
+      if (EEPROM.read(adressOn) != 0)
+      {
+        fileName = fileName + String((char)EEPROM.read(adressOn));
+      }
+      adressOn++;
+    }
+  }
+  else
+  {
+    arduboy.readUnitName(unitName);
+    fileName = unitName;
+  }
+
+  arduboy.setCursor(64, file*10+6);
+  arduboy.print((String)fileName);
+}
+
+void loadFileHighScore()
+{
+	EEPROM.write(CURRENT_HIGH_SCORE_ADDRESS, EEPROM.read(LOADED_FILE_NUMBER_ADDRESS + EEPROM.read(LOADED_FILE_NUMBER_ADDRESS)));
+	gameHighScore = EEPROM.read(LOADED_FILE_NUMBER_ADDRESS + EEPROM.read(LOADED_FILE_NUMBER_ADDRESS));
+}
+
+void resetCurrentFile()
+{
+	gameHighScore = 0;
+	EEPROM.write(CURRENT_HIGH_SCORE_ADDRESS, 0);
+	EEPROM.write(LOADED_FILE_NUMBER_ADDRESS + EEPROM.read(LOADED_FILE_NUMBER_ADDRESS), 0);
+}
+
+void refreshEraseScreen()
+{
+	arduboy.clear();
+
+	arduboy.setCursor(6, 3);
+  arduboy.print("Erase File?");
+  arduboy.drawFastHLine(5, 11, 67, WHITE);
+
+  arduboy.setCursor(16, 16);
+  arduboy.print("Yes");
+  arduboy.setCursor(16, 26);
+  arduboy.print("No");
+
+  arduboy.display();
+}
+
+void drawNameSelectionScreen(int fileSelected)
+{
+  const char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int selectedCharacter = -1;
+  int charOn = 0;
+  const int characterArrayLength = 26;
+  String selectedCharacters = "";
+  boolean rapidScroll = false;
+
+  refreshNameSelectionScreen(charOn, selectedCharacter, characters);
+
+  debounceButtons();
+  while(!arduboy.pressed(RIGHT_BUTTON) && !arduboy.pressed(LEFT_BUTTON))
+  {
+    if (arduboy.pressed(DOWN_BUTTON))
+    {
+      selectedCharacter--;
+      if (selectedCharacter < -1)
+      {
+        selectedCharacter = characterArrayLength-1;
+      }
+
+      arduboy.drawChar(23*charOn + 7, 0, characters[selectedCharacter], WHITE, 0, 4);
+      debounceButtons();
+    }
+
+    if (arduboy.pressed(UP_BUTTON))
+    {
+      selectedCharacter++;
+      if (selectedCharacter > characterArrayLength-1)
+      {
+        selectedCharacter = -1;
+      }
+      arduboy.drawChar(23*charOn + 7, 0, characters[selectedCharacter], WHITE, 0, 4);
+      debounceButtons();
+    }
+
+    if (arduboy.pressed(B_BUTTON))
+    {
+      charOn++;
+      if (charOn > 4)
+      {
+        charOn = 4;
+      }
+      debounceButtons();
+      selectedCharacters = selectedCharacters + characters[selectedCharacter];
+      arduboy.drawChar(23*charOn + 7, 0, characters[selectedCharacter], WHITE, 0, 4);
+    }
+
+    if (selectedCharacter < 0)
+    {
+      arduboy.drawChar(23*charOn + 7, 0, char(32), WHITE, 0, 4);
+    }
+
+    arduboy.display();
+  }
+
+  if (arduboy.pressed(RIGHT_BUTTON))
+  {
+    if (fileSelected == 2)
+    {
+      writeCharacters(selectedCharacters, 506, 510);
+    }
+
+    if (fileSelected == 3)
+    {
+      writeCharacters(selectedCharacters, 511, 515);
+    }
+  }
+
+  debounceButtons();
+  refreshLoadScreen();
+  arduboy.drawSlowXYBitmap(6,(fileSelected*10) + 7,arrow,8,6,1);
+  arduboy.display();
+}
+
+void refreshNameSelectionScreen(int charOn, int selectedCharacter, char characters[])
+{
+  arduboy.clear();
+  arduboy.drawFastHLine(5, 32, 22, WHITE);
+  arduboy.drawFastHLine(29, 32, 22, WHITE);
+  arduboy.drawFastHLine(53, 32, 22, WHITE);
+  arduboy.drawFastHLine(77, 32, 22, WHITE);
+  arduboy.drawFastHLine(101, 32, 22, WHITE);
+
+  arduboy.drawFastHLine(5, 33, 22, WHITE);
+  arduboy.drawFastHLine(29, 33, 22, WHITE);
+  arduboy.drawFastHLine(53, 33, 22, WHITE);
+  arduboy.drawFastHLine(77, 33, 22, WHITE);
+  arduboy.drawFastHLine(101, 33, 22, WHITE);
+
+  arduboy.setCursor(5, 40);
+  arduboy.print("B: Next Character");
+  arduboy.setCursor(5, 50);
+  arduboy.print("Right: Save Name");
+  arduboy.setCursor(5, 60);
+  arduboy.print("Left: Cancel");
+
+  arduboy.drawChar(23*charOn + 7, 0, characters[selectedCharacter], WHITE, 0, 4);
+}
+
+void writeCharacters(String charactersToWrite, int adressStart, int adressEnd)
+{
+  int adressOn = adressStart;
+  while (adressOn < adressEnd+1)
+  {
+    EEPROM.write(adressOn, charactersToWrite.charAt(adressOn-adressStart));
+    Serial.println((String)charactersToWrite.charAt(adressOn-adressStart) + ":" + (String)adressOn);
+    adressOn++;
+  }
+}
